d5a562410f5a2a18998fb4a2f7b47a1c666ae292
Jay <jai3500000@gmail.com>
2023-06-17 23:54:04 +0530

    Users Search fuction added

--- chat-app-frontend/src/Components/messageform/MessageForm.jsx
+++ chat-app-frontend/src/Components/messageform/MessageForm.jsx
@@ -6,21 +6,27 @@ export default function MessageForm() {
   function handleSubmit(e) {
     e.preventDefault();
   }
-  
-
 
   return (
     <div className="messageform">
       <div className="messageform_container">
         <div className="message_output"></div>
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
           <div className="send_message_bar">
             <input type="text" placeholder="Your message" />
             <button type="submit" className="btn btn-primary send_btn">
               <IoSend className="send_btn_icon" />
             </button>
           </div>
-        </form>
+        </form> */}
+        <div className="send_message_bar">
+          <form className="search-form form">
+            <input className="search-field" placeholder="Your Message..." />
+            <button className="search-submit button">
+              <IoSend className="send-button" />
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- chat-app-frontend/src/Components/sidebar/SideBar.jsx
+++ chat-app-frontend/src/Components/sidebar/SideBar.jsx
@@ -1,20 +1,80 @@
-import React from 'react'
-import "./sidebar.scss"
+import React, { useState } from "react";
+import "./sidebar.scss";
+import { RxCross2 } from "react-icons/rx";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 export default function SideBar() {
-  const rooms = ["first room", "second room", "third room"];
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const token = localStorage.getItem("token");
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const url = `http://localhost:8080/api/user?search=${search}`;
+      const { data } = await axios.get(url, config);
+      setSearchResult(data);
+    } catch (err) {
+      toast.error("Failed to load the search result", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    }
+  };
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    setSearch("");
+    setSearchResult([]);
+  }
 
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="sidebar_container">
-        <h2>Available Rooms</h2>
-        <ul className ="list-group">
-          {rooms.map((room , idx) => (
-            <li className='list-group-item' key={idx}>{room}</li>
-          ))}
-        </ul>
-        <h2>Members</h2>
+        <div className="searchbar">
+          <input
+            className="search_input"
+            type="text"
+            name=""
+            value={search}
+            placeholder="Search..."
+            onChange={handleSearch}
+          />
+          {!search ? null : (
+            <button className="search_icon" onClick={handleCancel}>
+              <RxCross2 />
+            </button>
+          )}
+          <ToastContainer autoClose={3000} />
+          {search ? (
+            <div className="search-results">
+              {searchResult.map((user) => (
+                <>
+                  <div key={user._id} className="search-result">
+                    <img src={user.avatarUrl} alt="profile pic" />
+                    <div className="userdetails">
+                      <h3>{user.fullName}</h3>
+                      <p>
+                        <b>Email: </b>
+                        {user.email}
+                      </p>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
-  )
+  );
 }
